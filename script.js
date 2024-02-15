a733e02504747f6b3340533b7adc87a3b5631964
Bernardo A <araujobernardo11111@gmail.com>
2024-02-15 10:38:41 -0300

    Projeto | Barbearia Web

--- script.js
+++ script.js
@@ -19,27 +19,75 @@ const barbearia = {
   
   
 function buscaCortePorId(id) {
+    for(let i = 0; i < barbearia.cortes.length; i++){
+        if(barbearia.cortes[i].id == id){
+            return barbearia.cortes[i]
+        }
+    }
+    return "Corte não encontrado"
 }
 
 function buscaBarbaPorId(id) {
+    for(let i = 0; i < barbearia.barbas.length; i++){
+        if(barbearia.barbas[i].id == id){
+            return barbearia.barbas[i]
+        }
+    }
+    return "Barba não encontrada"
 }
 
 function verificaStatusBarbearia() {
+    if(barbearia.estaAberto){
+        return "Estamos abertos"
+    }
+        return "Estamos fechados"
 }
 
 function retornaTodosCortes() {
+    return barbearia.cortes
 }
 
 function retornaTodasBarbas() {
+    return barbearia.barbas
 }
 
 function criaPedido(nomeCliente, corteId, barbaId) {
+let corte = buscaCortePorId(corteId);
+let barba = buscaBarbaPorId(barbaId);
+let pedido = {
+    nome: nomeCliente,
+    pedidoCorte: corte.tipo,
+    pedidoCortePreco: corte.valor,
+    pedidoBarba: barba.tipo,
+    pedidoBarbaPreco: barba.valor,
+}
+    return pedido
 }
 
 function atualizarServico(lista, id, valor, tipo) {
+    if(lista = barbearia.cortes){
+        for(let i = 0; i < barbearia.cortes.length; i++){
+            if(barbearia.cortes[i].id  === id){
+                barbearia.cortes[i].tipo = tipo;
+                barbearia.cortes[i].valor = valor;
+                return barbearia.cortes
+            }
+        }
+    }else if(lista = barbearia.barbas){
+        for(let i = 0; i < barbearia.barbas.length; i++){
+            if(barbearia.barbas[i].id === id){
+                barbearia.barbas[i].tipo = tipo;
+                barbearia.barbas[i].valor = valor;
+                return barbearia.barbas
+            }
+        }
+    }
+    return barbearia
 }
 
 function calculaTotal(pedido) {
+    const precoCorte = pedido.pedidoCortePreco;
+    const precoBarba = pedido.pedidoBarbaPreco;
+    const total = precoBarba + precoCorte
+    return total
 }
-  
-  
